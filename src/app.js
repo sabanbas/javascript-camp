60e69b6bf4a601d16f377eb28fdcb3a1dd9a6d07
sabanbas <160129425+sabanbas@users.noreply.github.com>
2024-11-08 22:07:56 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+
+
+let konutKredileri = ["Konut kredisi","Emlak kredisi","Araç kredisi","İhtiyaç kredisi"]
+//Bir değişkene birden fazla değer atamak için kare parantez kullanılır.
+
+console.log("<ul>")
+for (let i = 0;i<konutKredileri.length;i++){//i değişkenine 0 değer vererek konurKredileri değişkenindeki tüm değerleri basar
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+//konut kredi çeşitlerini döngüye sokarak sıralar
+//for döngü başlatma işlemini yapar.
+
+
+
+let hayatSigortalari = ["Genel kapsamlı sigorta","Sağlık sigortası","İşsizlik sigortası","Aile kapsamlı sigorta"]
+
+console.log("<ul>")
+for (let i = 0;i<hayatSigortalari.length;i++){
+    console.log("<li>"+hayatSigortalari[i]+"</li>")
+}
+console.log("</ul>")
+
+let tasitSigortalari = ["Dolu hasarı","Kapsamlı kaza sigortası","Sel sigortası","Tam kapsamlı sigorta"]
+
+console.log ("<ul>")
+for (let i=0;i<tasitSigortalari.length;i++) {
+    console.log("<li>"+tasitSigortalari[i]+"</li>")
+
+}
+console.log("</ul>")
